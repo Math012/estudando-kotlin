620db6b9ce4a00d7f4ab7e1c357fd9befef39187
Math012 <math111.0811@gmail.com>
2025-03-20 16:14:45 -0300

    Tipos de variaveis

---
+++ src/variaveis_em_kotlin/Variaveis.kt
@@ -0,0 +1,46 @@
+package variaveis_em_kotlin
+
+fun main(){
+
+    // Números decimais
+    var kks1: Float
+    var kks2: Double
+
+    // Declarando valores decimais
+    var kkss1: Float = 500.00F
+    var kkss2: Double = 1500.00
+
+    // Números
+    var n1: Byte;
+    var n2: Short;
+    var n3: Int;
+    var n4: Long;
+    var n5: UByte
+    var n6: UShort
+    var n7: UInt
+    var n8: ULong
+
+    // Declarando valores
+    var nn1: Byte = 100;
+    var nn2: Short = -32;
+    var nn3: Int = 10000;
+    var nn4: Long = 99999999;
+    var nn5: UByte = 255u
+    var nn6: UShort = 33u
+    var nn7: UInt = 4294967295u
+    var nn8: ULong = 18446744073709551615u
+
+    // Podemos ultrapassar a faixa de valor utilizando o recurso
+    // unsigned (anotação "u")
+
+    // Declarando valores sem especificar o tipo
+
+    var nnn1 = 100;
+    var nnn2 = -32;
+    var nnn3 = 10000;
+    var nnn4 = 99999999;
+    var nnn5 = 255u
+    var nnn6 = 33u
+    var nnn7 = 4294967295u
+    var nnn8 = 18446744073709551615u
+}
