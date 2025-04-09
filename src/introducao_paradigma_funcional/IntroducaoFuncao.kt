d043577e37aa4767de21ce1d86a58059f2e4328d
Math012 <math111.0811@gmail.com>
2025-04-09 14:34:42 -0300

    introdução de funções em Kotlin e tipos de retorno

---
+++ src/introducao_paradigma_funcional/IntroducaoFuncao.kt
@@ -0,0 +1,18 @@
+package introducao_paradigma_funcional
+
+class Usuario{
+    var nome = "Matheus"
+
+    fun verificarUsuarioLogado(){
+
+    }
+
+}
+
+fun somar1(){
+    println(2 + 5)
+}
+
+fun main(){
+    somar()
+}
---
+++ src/introducao_paradigma_funcional/RetornoDeFuncoes.kt
@@ -0,0 +1,11 @@
+package introducao_paradigma_funcional
+
+
+fun somar() : Int{
+    return 5 + 5
+}
+
+fun main(){
+    val retorno = somar()
+    println(retorno * 2)
+}
