932ecca4b838b70d97833a3aaa0d8c66d5fb52d6
Math012 <math111.0811@gmail.com>
2025-04-12 20:59:26 -0300

    Exemplificando a função lambda do button.onClickListener

--- src/funcoes_lambdas/FuncoesLambdas.kt
+++ src/funcoes_lambdas/FuncoesLambdas.kt
@@ -1,7 +1,25 @@
 package funcoes_lambdas
 
+class Botao{
+
+    // criando uma função que não possui retorno e que pode executar qualquer ação do usuário
+    fun configurarClickBotao(funcao: () -> Unit){
+        // chamando a função configurarClickBotao() para que ela seja executado
+        funcao()
+    }
+}
+
 
 fun main(){
+
+    val botao = Botao();
+
+    // Criando uma função lambda para executar o método
+
+    botao.configurarClickBotao {
+        println("Executou o botão através da função lambda")
+    }
+
 
     // criando a função lambda e atribuindo a função em uma variavel
     val funcaoLambda = {
