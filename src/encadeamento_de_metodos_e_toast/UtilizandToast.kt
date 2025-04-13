fa967a8b7822d529ea33f5af1e28f9cb02ee40bd
Math012 <math111.0811@gmail.com>
2025-04-12 21:12:21 -0300

    Implementação do Toast (android)

---
+++ src/encadeamento_de_metodos_e_toast/UtilizandToast.kt
@@ -0,0 +1,14 @@
+package encadeamento_de_metodos_e_toast
+
+/*
+
+
+EXEMPLO PARA UTILIZAR TOAST
+
+
+
+    Toast.makeText(context: this, "mensagem que deseja exibir", Toast.LENGTH_LONG).show
+
+
+
+ */
